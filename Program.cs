69e597b99afc41a58d60cb87cf11f32391d3b1c6
Jessica White <me@jesswhite.co.uk>
2018-06-17 15:45:32 +0100

    Fix to Start up ports

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace response_api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>();
     }
 }
